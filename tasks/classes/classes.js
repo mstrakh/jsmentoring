7c070d147b0f46c063fc0ea65bc0d53ea1ca5f9f
Maximus Tremor <maximustremor@gmail.com>
2020-07-15 13:52:05 +0100

    Homework for classes and pageObject imitation done.

--- tasks/classes/classes.js
+++ tasks/classes/classes.js
@@ -1,10 +1,20 @@
+const { expect } = require("chai");
+
 /**
  * implement class Person with:
  * 1) properties firstName and lastName
  * 2) constructor that pass firstName and lastName to the object
  * 3) method getFullName
  */
-//put your code here
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
 
 /**
  * implement class Student that extends Person with:
@@ -12,10 +22,18 @@
  * 2) constructor that pass grade to the object
  * 3) method getGrade that returns property grade
  */
-//put your code here
+class Student extends Person {
+    constructor(firstName, lastName, grade) {
+        super(firstName, lastName);
+        this.grade = grade;    
+    }
+    getGrade() {
+        return this.grade;
+    }
+}
 
-const person = //create instance of class person
-const student = //create instance of class student
+const person = new Person('Joe', 'Madison'); //create instance of class person
+const student = new Student('Sam', 'Smith', 'A'); //create instance of class student
 
 module.exports = {
     person,
